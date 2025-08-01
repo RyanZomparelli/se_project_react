7a4cf9a96b58d902e129b3e6a041d7b6a141ddd3
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-08-01 10:52:06 -0400

    feat(footer): build footer component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Header from "../Header/Header.jsx";
+import Footer from "../Footer/Footer.jsx";
 
 function App() {
+  const [Weather, setWeather] = useState();
+
   return (
     <div className="page">
       <Header />
+      <Footer />
     </div>
   );
 }
