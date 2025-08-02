a16ae2e20c76fef07f7da56179caeaac07261512
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-08-01 21:17:02 -0400

    (utils): add data.js with weatherCards array and defaultClothingItems array and add all possible weather cards to assets

---
+++ src/utils/data.js
@@ -0,0 +1,101 @@
+export const weatherCards = [
+  {
+    isDay: true,
+    weather: "sunny",
+    src: "../assets/sunny.png",
+  },
+  {
+    isDay: true,
+    weather: "cloudy",
+    src: "../assets/cloudy.png",
+  },
+  {
+    isDay: true,
+    weather: 'rain',
+    src: '../assets/rain.png',
+  },
+  {
+    isDay: true,
+    weather: 'storm',
+    src: '../assets/storm.png',
+  },
+  {
+    isDay: true,
+    weather: 'snow',
+    src: '../assets/snow.png'
+  },
+  {
+    isDay: true,
+    weather: 'fog',
+    src: '../assets/fog.png'
+  },
+  {
+    isDay: false,
+    weather: 'clear',
+    src: '../assets/night_clear.png',
+  },
+  {
+    isDay: false,
+    weather: 'cloudy'
+    src: '../assets/night_cloudy.png',
+  },
+  {
+    isDay: false,
+    weather: 'rain',
+    src: '../assets/night_rain.png',
+  },
+  {
+    isDay: false,
+    weather: 'storm',
+    src: '../assets/night_storm.png',
+  },
+  {
+    isDay: false,
+    weather: 'snow',
+    src: '../assets/night_snow',
+  },
+  {
+    isDay: false,
+    weather: 'fog',
+    src: '../assets/night_fog.png'
+  },
+];
+
+export const defaultClothingItems = [
+  {
+    _id: 0,
+    name: "Cap",
+    weather: "hot",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Cap.png?etag=f3dad389b22909cafa73cff9f9a3d591",
+  },
+  {
+    _id: 1,
+    name: "Hoodie",
+    weather: "warm",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Hoodie.png?etag=5f52451d0958ccb1016c78a45603a4e8",
+  },
+  {
+    _id: 2,
+    name: "Jacket",
+    weather: "cold",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Jacket.png?etag=f4bb188deaa25ac84ce2338be2d404ad",
+  },
+  {
+    _id: 3,
+    name: "Sneakers",
+    weather: "cold",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Sneakers.png?etag=3efeec41c1c78b8afe26859ca7fa7b6f",
+  },
+  {
+    _id: 4,
+    name: "T-Shirt",
+    weather: "hot",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/T-Shirt.png?etag=44ed1963c44ab19cd2f5011522c5fc09",
+  },
+  {
+    _id: 5,
+    name: "Coat",
+    weather: "cold",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/wtwr-project/Coat.png?etag=298717ed89d5e40b1954a1831ae0bdd4",
+  }
+]
