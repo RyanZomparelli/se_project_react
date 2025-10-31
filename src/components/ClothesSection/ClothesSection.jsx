81f94a9cb9a7ba1b9bf363ccf7eb41eedfd7a508
Ryan Zomparelli <zomp.ryan@gmail.com>
2025-10-30 21:22:49 -0400

    feat(CurrentUserContext): subscribe to user context in ClothesSection and ItemModal for conditional rendering.

--- src/components/ClothesSection/ClothesSection.jsx
+++ src/components/ClothesSection/ClothesSection.jsx
@@ -1,14 +1,24 @@
+import { useContext } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
+
 const ClothesSection = ({ clothingItem, handleItemCardClick }) => {
+  const { currentUser } = useContext(CurrentUserContext);
+  const isOwnCard = clothingItem.owner === currentUser.user._id;
+
   return (
-    <li className="clothing__card">
-      <p className="clothing__card-title">{clothingItem.name}</p>
-      <img
-        src={clothingItem.imageUrl}
-        alt={clothingItem.name}
-        className="clothing__card-image"
-        onClick={() => handleItemCardClick(clothingItem)}
-      />
-    </li>
+    <>
+      {isOwnCard && (
+        <li className="clothing__card">
+          <p className="clothing__card-title">{clothingItem.name}</p>
+          <img
+            src={clothingItem.imageUrl}
+            alt={clothingItem.name}
+            className="clothing__card-image"
+            onClick={() => handleItemCardClick(clothingItem)}
+          />
+        </li>
+      )}
+    </>
   );
 };
 
--- src/components/ItemModal/ItemModal.jsx
+++ src/components/ItemModal/ItemModal.jsx
@@ -1,3 +1,5 @@
+import { useContext } from "react";
+import CurrentUserContext from "../../contexts/CurrentUserContext";
 import "./ItemModal.css";
 
 export default function ItemModal({
@@ -6,19 +8,23 @@ export default function ItemModal({
   onOverlayClick,
   onModalOpen,
 }) {
+  const { currentUser } = useContext(CurrentUserContext);
+  const isOwnCard = card.owner === currentUser.user._id;
+
   return (
     <section className="modal" onClick={onOverlayClick}>
       <div className="modal__container">
         <button className="item-modal__btn-close" onClick={onClose}></button>
         <img src={card.imageUrl} alt={card.name} className="item-modal__img" />
         <div className="item-modal__items">
-          <button
-            className="item-modal__btn-delete"
-            onClick={() => onModalOpen("delete-item")}
-          >
-            Delete item
-          </button>
-
+          {isOwnCard && (
+            <button
+              className="item-modal__btn-delete"
+              onClick={() => onModalOpen("delete-item")}
+            >
+              Delete item
+            </button>
+          )}
           <p className="item-modal__name">{card.name}</p>
           <p className="item-modal__weather">Weather: {card.weather}</p>
         </div>
